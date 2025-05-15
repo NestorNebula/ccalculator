515febea7519997afc32a3fb1f44c64366ccca34
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2025-05-15 16:31:06 +0200

    feat: operations module's header file

---
+++ include/operations.h
@@ -0,0 +1,41 @@
+#ifndef OPERATIONS_H
+#define OPERATIONS_H
+
+typedef double Number;
+
+/*
+ * add:
+ * Adds n1 and n2 and returns the sum.
+ */
+Number add(Number n1, Number n2);
+
+/*
+ * subtract:
+ * Subtracts n1 and n2 and returns the difference.
+Number subtract(Number n1, Number n2);
+
+/*
+ * multiply:
+ * Multiplies n1 and n2 and returns the product.
+ */
+Number multiply(Number n1, Number n2);
+
+/*
+ * divide:
+ * Divides n1 and n2 and returns the quotient.
+ */
+Number divide(Number n1, Number n2);
+
+/*
+ * power:
+ * Returns n to the power of p.
+ */
+Number power(Number n, Number p);
+
+/*
+ * square_root:
+ * Returns the square root of n or -1 if n is negative.
+ */
+Number square_root(Number n);
+
+#endif
