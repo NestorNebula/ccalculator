1f848a4cd6ac02af4192cc52e192c00c60020b14
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2025-05-19 17:06:21 +0200

    feat: add variable argument list to functions

--- include/error.h
+++ include/error.h
@@ -2,14 +2,16 @@
 #define ERROR_H
 
 #include <stdbool.h>
+#include <stdarg.h>
 
 /*
  * set_error:
- * Sets error status to active and error description to err.
+ * Sets error status to active and 
+ * sets error description to err, formatted with arg, if any.
  * Returns a pointer to the error's description or a null pointer
  * if no space was available to store the description.
  */
-char *set_error(const char *err);
+char *set_error(const char *err, va_list arg); 
 
 /*
  * clear_error:
@@ -33,8 +35,8 @@ void print_error(FILE *stream);
 /*
  * exit_with_error:
  * A shortcut for set_error(err), print_error() and exit(EXIT_FAILURE).
- * Prints err to stream before exiting the program.
+ * Prints err, formatted with arg, if any, to stream before exiting the program.
  */
-void exit_with_error(const char *err, FILE *stream);
+void exit_with_error(FILE *stream, const char *err, va_list arg);
 
 #endif
