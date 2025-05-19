a5f991fc2e0e18d7a264dc1528b0682cad047aa0
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2025-05-19 17:11:57 +0200

    test: add tests for variable argument functions

--- tests/error.cpp
+++ tests/error.cpp
@@ -10,6 +10,10 @@ TEST(set_error, returns_pointer_to_error_description) {
   EXPECT_STREQ("Error", set_error("Error"));
 }
 
+TEST(set_error, works_with_formatted_errors) {
+  EXPECT_STREQ("Error 418", set_error("Error %d", 418);
+}
+
 TEST(clear_error, returns_pointer_to_empty_description) {
   EXPECT_STREQ("", clear_error());
 }
@@ -41,8 +45,19 @@ TEST(exit_with_error, prints_error_description_in_stream) {
   char err[] = "Error";
   char err_cpy[sizeof(err)];
 
-  EXPECT_EXIT(exit_with_error(err, stream),
+  EXPECT_EXIT(exit_with_error(stream, err),
               ::testing::ExitedWithCode(EXIT_FAILURE), "");
+  rewind(stream);
+  fgets(err_cpy, sizeof(err_cpy), stream);
+  EXPECT_STREQ(err, err_cpy);
+}
+
+TEST(exit_with_error, works_with_formatted_errors) {
+  FILE *stream = tmpfile();
+  char err[] = "Error 418";
+  char err_cpy[sizeof(err)];
+
+  exit_with_error(stream, "Error %d", 418);
   rewind(stream);
   fgets(err_cpy, sizeof(err_cpy), stream);
   EXPECT_STREQ(err, err_cpy);
